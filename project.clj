e6801c322645597f168650c3d34652b90603aa2a
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-08-14 20:59:27 +0300

    prepare for auto-deployment to clojars

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "https://github.com/mrdimosthenis/Synapses"
   :license {:name "MIT"
             :url  "https://rem.mit-license.org/"}
+  :scm {:name "git" :url "https://github.com/mrdimosthenis/clj-synapses"}
   :dependencies [[com.github.mrdimosthenis/synapses_3 "8.0.0"]
                  [org.clojure/clojure "1.10.3"]]
   :profiles {:test {:resource-paths ["test-resources"]
-                    :dependencies   [[org.clojure/data.csv "1.0.0"]]}})
+                    :dependencies   [[org.clojure/data.csv "1.0.0"]]}}
+  :pom-addition [:developers
+                 [:developer
+                  [:id "mrdimosthenis"]
+                  [:name "Dimos Michailidis"]]])
