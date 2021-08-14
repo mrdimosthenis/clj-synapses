02ee8bb7d08ed0e5db03cebc0c147f2fbfb6f02d
mrdimosthenis <mrdimosthenis@hotmail.com>
2021-08-14 03:35:08 +0300

    add developer information

--- project.clj
+++ project.clj
@@ -2,13 +2,18 @@
   :description "A neural networks library for Clojure"
   :url "https://github.com/mrdimosthenis/Synapses"
   :license {:name "MIT"
-            :url "https://rem.mit-license.org/"}
+            :url  "https://rem.mit-license.org/"}
   :scm {:name "git" :url "https://github.com/mrdimosthenis/synapses-clj"}
   :dependencies [[com.github.mrdimosthenis/synapses_3 "8.0.0"]
                  [org.clojure/clojure "1.10.3"]]
   :profiles {:test {:resource-paths ["test-resources"]
-                   :dependencies [[org.clojure/data.csv "1.0.0"]]}}
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}
-                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]])
+                    :dependencies   [[org.clojure/data.csv "1.0.0"]]}}
+  :deploy-repositories
+  [["releases" {:url   "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                :creds :gpg}
+    "snapshots" {:url   "https://oss.sonatype.org/content/repositories/snapshots/"
+                 :creds :gpg}]]
+  :pom-addition [:developers
+                 [:developer
+                  [:id "mrdimosthenis"]
+                  [:name "Dimos Michailidis"]]])
